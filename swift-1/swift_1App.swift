b671dd826172b37f1af7708cefefafead88cf319
Wilber Alegria <wilber.alegria99@gmail.com>
2024-09-28 14:13:57 -0600

    Initial Commit

---
+++ swift-1/swift_1App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_1App.swift
+//  swift-1
+//
+//  Created by Wilber Alegria on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-1Tests/swift_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  swift_1Tests.swift
+//  swift-1Tests
+//
+//  Created by Wilber Alegria on 28/09/24.
+//
+
+import Testing
+@testable import swift_1
+
+struct swift_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
